e868638b74bda96395ca7c1610b32af7d968d632
Levashov-VV <levashov.slava99@mail.ru>
2024-04-22 15:37:44 +0300

    Исправление лексики

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,106 @@
-import { useState } from 'react'
-import Input from "./components/Input/Input"
-import './App.css'
-import Check from "./assets/Check.svg"
-import Busket from "./assets/TrashSimple.svg"
+import { useState } from 'react';
+import Input from "./components/Input/Input";
+import './App.css';
+import Check from "./assets/Check.svg";
+import Busket from "./assets/TrashSimple.svg";
 
 
 function App() {
-  const [task, setTask] = useState([]);
-  const [doneTask] = useState([]);
-  const insertTask = (value) =>{
-    if(value){
-      setTask([...task, {id: Date.now(), text: value}])
-    }
-    else{
+  
+  const [tasks, setTasks] = useState([]);
+  const [doneTasks] = useState([]);
+
+  const addTask = (value) => {
+    if (value) {
+      setTasks([...tasks, {id: Date.now(), text: value}])
+    } else {
       alert("Введите в поле ввода задачу")
     }
-  }
-  function removeTask(id){
-    setTask(task.filter(task => task.id !== id))
-  }
-  function addTask(id, text){
-    doneTask.push({
+  };
+
+  const removeTask = (id) => {
+    const nextTasks = tasks.filter(task => task.id !== id);
+    setTasks(nextTasks);
+  };
+
+  const doneTask = (id, text) => {
+    doneTasks.push({
       id: id,
       text: text
-    })
-    removeTask(id)
-  }
+    });
+    
+    removeTask(id);
+  };
+  
+  const onClickDone = (e) => {
+    e.stopPropagation();
+    doneTask(tasks.id, tasks.text);
+  };
+  
+  const onClickDelete = (e) => {
+    e.stopPropagation();
+    removeTask(tasks.id);
+  };
+
   return (
     <div className="todo-list">
-        <Input 
-          insertTask={insertTask}
-        />
-        <div className="todo-tasks">
-          <div className="title">Tasks to do - {task.length} </div>
-          <div className="tasks-block">
-            {
-              task.map(task =>{
-                return(
-                  <div className='task' key={task.id}>
-                    <div className="task-text task">{task.text}</div>
-                    <div className="task-btn">
-                      <button className="btn" type="button" onClick={e => {
-                        e.stopPropagation()
-                        addTask(task.id, task.text)
-                      }}>
-                      <img className="task-img" src={Check} alt="Check"/>
-                      </button>
-                      <button className="btn" type="button" onClick={e => {
-                        e.stopPropagation()
-                        removeTask(task.id)
-                      }}>
-                      <img className="task-img" src={Busket} alt="busket"/>
-                      </button>
-                    </div>
-                  </div>
-                )
-              })
-            }
-            </div>
-          </div>
-        <div className="todo-tasks done">
-          <div className="title">Done - {doneTask.length}</div>
-          {
-              doneTask.map(doneTask =>{
-                return(
-                  <div className='task done-text' key={doneTask.id}>
-                    {doneTask.text}
-                    </div>
-              )})
-              
-          }
+      <Input addTask={addTask} />
+
+      <div className="todo-tasks">
+        <div className="title">
+          Tasks to do - {tasks.length}
         </div>
-      </div>
-  )
+
+        <div className="tasks-block">
+          {tasks.map(task => (
+            <div className='task' key={task.id}>
+              <div className="task-text task">
+                {task.text}
+              </div>
+
+              <div className="task-btn">
+                <button
+                  className="btn"
+                  type="button"
+                  onClick={onClickDone}
+                >
+                  <img
+                    className="task-img"
+                    src={Check}
+                    alt="Check"
+                  />
+                </button>
+
+                <button
+                  className="btn"
+                  type="button"
+                  onClick={onClickDelete}
+                >
+                  <img
+                    className="task-img"
+                    src={Busket}
+                    alt="busket"
+                  />
+                </button>
+              </div>
+            </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="todo-tasks done">
+            <div className="title">
+              Done - {doneTasks.length}
+            </div>
+
+            {doneTasks.map(doneTask => (
+              <div className='task done-text' key={doneTask.id}>
+                {doneTask.text}
+              </div>
+            ))}
+          </div>
+          </div>
+  );
 }
+
 export default App
