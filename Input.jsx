b87f5d0f0ae5bbcb6d6e2e70f01a1c5ec8b17fca
Levashov-VV <132905799+Levashov-VV@users.noreply.github.com>
2024-05-08 17:48:07 +0300

    LocalStorage

---
+++ Input.jsx
@@ -0,0 +1,52 @@
+import Plus from '../../assets/Plus.svg';
+import { useState } from 'react';
+import './style.css';
+
+const Input = ({
+  addTask,
+}) => {
+  const [value, setValue] = useState('');
+
+  const handleClick = () => {
+    const val = '';
+    addTask(value);
+    setValue(val);
+    localStorage.getItem('tasks', JSON.stringify(val))
+  };
+  
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  };
+
+  const toPressEnter = (e) => {
+    if(e.key === 'Enter') {
+      handleClick()
+    }
+  };
+
+  return (
+    <div className="container">
+      <input
+        className="input" 
+        type="text" 
+        placeholder="Add a new task"
+        value={value} 
+        onChange={handleChange}
+        onKeyUp={toPressEnter}
+      />
+
+      <button
+        className="container-btn"
+        type="button" 
+        onClick={handleClick}
+      >
+        <img
+          src={Plus}
+          alt="Plus" 
+        />
+      </button>
+    </div>
+  );
+}
+
+export default Input;
